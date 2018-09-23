686fab8532810d4db97d359e4bb27891a7f03c91
Андрій Білецький < >
2018-09-23 14:45:46 +0300

    made it optimal

--- index.js
+++ index.js
@@ -1,305 +1,276 @@
 const R = require('ramda')
-
-const ORDER_TYPES = {
-  EMPTY: 'EMPTY',
-  ONE_ACTION: 'ONE_ACTION',
-  SEQUENCE: 'SEQUENCE',
-  PARALLEL: 'PARALLEL'
+// doAction :: async (data, state, amount) => res
+const DO_NOTHING = () => {}
+const INITIAL_COSTS = 1
+const INITIAL_MONEY = 0
+const EMPTY_NODE_DATA = Symbol('EMPTY_NODE')
+const ERRORS_PROP = Symbol('ERRORS')
+const ERROR_TYPE_PROP = Symbol('ERROR_TYPE_PROP')
+const ERROR_CATCHER = Symbol('ERROR_CATCHER')
+const IS_IMPOSSIBLE = Symbol('IMPOSSIBLE')
+const TIMEOUT_TOKEN = Symbol('TIMEOUT_TOKEN')
+const ABORTED = Symbol('ABORTED')
+const wait = ms => new Promise(res => setTimeout(() => res(TIMEOUT_TOKEN), ms))
+const createTimeoutPromise = (timeout, promise) => {
+  const resPromise = Promise.race([promise, wait(timeout)])
+  .then(res => {
+    if (res === TIMEOUT_TOKEN) throw new Error('Timeout was overflowed')
+    return res
+  })
+  return resPromise
 }
+const getAllErrors = state => state[ERRORS_PROP] || {}
 
-const getEmpty = () => ({ type: ORDER_TYPES.EMPTY })
-const getOneAction = task => ({ type: ORDER_TYPES.ONE_ACTION, task })
-const colorPropName = Symbol('COLOR')
-const getSequence = (...scenarios) => {
-  const innerScenarios = scenarios.reduce((res, s) => {
-    switch (s.type) {
-      case ORDER_TYPES.EMPTY:
-        return res
-      case ORDER_TYPES.ONE_ACTION:
-      case ORDER_TYPES.PARALLEL:
-        return [...res, s]
-      case ORDER_TYPES.SEQUENCE:
-        return [...res, ...s.scenarios]
-    }
-  }, [])
-  switch (innerScenarios.length) {
-    case 0: return getEmpty()
-    case 1: return R.head(innerScenarios)
-    default: return {
-      type: ORDER_TYPES.SEQUENCE,
-      scenarios: innerScenarios
-    }
-  }
-}
-
-const getParallel = (...scenarios) => {
- const innerScenarios = scenarios.reduce((res, s) => {
-    switch (s.type) {
-      case ORDER_TYPES.EMPTY:
-        return res
-      case ORDER_TYPES.ONE_ACTION:
-      case ORDER_TYPES.SEQUENCE:
-        return [...res, s]
-      case ORDER_TYPES.PARALLEL:
-        return [...res, ...s.scenarios]
-    }
-    throw s
-  }, [])
-  switch (innerScenarios.length) {
-    case 0:
-      return getEmpty()
-    case 1:
-      return R.head(innerScenarios)
-    default:
-      return {
-        type: ORDER_TYPES.PARALLEL,
-        scenarios: innerScenarios
-      }
-  } 
-}
-
-const componentToScenario = R.curry((isDone, component) => {
-  let lastTrees       = component
-  const sequence      = [],
-        isIndependent = R.pipe(
-          R.pathOr([], ['before']),
-          R.all(id => isDone[id])
-        )
-  while (lastTrees.length > 0) {
-    const [heads, tails] = R.partition(isIndependent, lastTrees)
-    if (heads.length === 0) {
-      throw 'Something wrong'
-    }
-    const parallel = getParallel(
-      ...heads.map(({ data }) => getOneAction(data))
-    )
-    sequence.push(parallel)
-    heads.forEach(({id}) => {
-      isDone[id] = true
-    })
-    lastTrees = tails
-  }
-  return getSequence(...sequence)
+const getError = R.curry((state, id) => {
+  const allErrors = getAllErrors(state)
+  return R.path([id, 0], allErrors)
 })
 
-class Forest {
-  constructor () {
-    this.trees = []
-    this._dict = []
-  }
-
-  getTreeById (id) {
-    return R.path([id], this._dict)
-  }
-
-  get treeIds () {
-    return Object.keys(this._dict)
-  }
-
-  appendTree (tree) {
-    if (this._dict[tree.id]) {
-      this._dict[tree.id] = tree
-      this.trees = this.trees.map(t => t.id === tree.id ? tree : t)
-      return
-    }
-    this.trees.push(tree)
-    this._dict[tree.id] = tree
-    tree.before.forEach(id => {
-      this._dict[id].after.push(tree.id)
-    })
-    tree.after.forEach(id => {
-      this._dict[id].before.push(tree.id)
-    })
-    return this
-  }
-
-  appendTrees (trees) {
-    let needToBeAdded = trees
-    const isReady = R.pipe(
-      R.pathOr([], ['before']),
-      R.all(id => this._dict[id])
-    )
-    while (needToBeAdded.length > 0) {
-      const [ready, notReady] = R.partition(
-        isReady,
-        needToBeAdded
-      )
-      if (R.isEmpty(ready)) {
-        const notReadyIds = R.pluck('id', notReadyIds)
-        throw new Error(`Unresolved ${notReadyIds.join(', ')}`)
-      }
-      ready.forEach(t => this.appendTree(t))
-      needToBeAdded = notReady
-    }
-    return this
-  }
-
-  get getComponents () {
-    let color = 0
-    const getColor          = R.prop(colorPropName)
-    const isColored         = R.pipe(getColor, R.complement(R.isNil))
-    const notColored        = R.complement(isColored)
-    const setColor          = R.curry((color, tree) => {
-      tree[colorPropName] = color 
-    })
-    const colorAllNeighbors = R.curry((color, tree) => {
-      if (isColored(tree)) return
-      setColor(color, tree)
-      const idsToBeColored = [...tree.after, ...tree.before]
-      idsToBeColored.map(id => this.getTreeById(id))
-        .forEach(t => colorAllNeighbors(color, t))
-    })
-
-    while (true) {
-      const firstNotColored = this.trees.find(notColored)
-      if (!firstNotColored) break;
-      colorAllNeighbors(color, firstNotColored)
-      color++
-    }
-
-    let components = []
-    for (let c = 0; c < color; c++) {
-      const treesInComponent = this.trees.filter(
-        R.pipe(
-          getColor,
-          R.equals(c)
-        )
-      ).map(R.omit(colorPropName))
-
-      components.push(treesInComponent)
-    }
-    return components
-  }
-
-  get toScenario () {
-    const isDone = {}
-    const ids = this.treeIds
-    switch (ids.length) {
-      case 0:
-        return getEmpty()
-      case 1:
-        const tree = R.head(this.trees)
-        return getOneAction(tree.data)
-    }
-
-    const components = this.getComponents
-    if (R.isEmpty(components)) return getEmpty()
-    if (components.length === 1) {
-      const [component] = components
-      return componentToScenario(isDone, component)
-    }
-    const parallel = components.map(componentToScenario(isDone))
-    return getParallel(...parallel)
-  }
-}
-
-class Tree {
-  constructor(id = undefined['id required'], data = null) {
+class Node {
+  constructor (id, data, costs = INITIAL_COSTS, initialMoney = 0, getPromiseCatcher = null, maxGive=Infinity) {
     this.id = id
     this.data = data
-    this.before = []
-    this.after = []
+    this.money = initialMoney
+    this.costs = costs
+    this.errorChildren = []
+    this.children = []
+    this.maxGive = maxGive
+    this.getPromiseCatcher = getPromiseCatcher
+    this[ABORTED] = false
+    this.abortedPromise = new Promise((res, rej) => {
+      this._abortResolveFunc = res
+    })
+    this.createPromise()
   }
-  setBefore(newBefore) {
-    this.before = newBefore
+  isAborted () {
+    return this[ABORTED]
   }
-}
+  setAborted (value = true) {
+    if (value) this._abortResolveFunc(ABORTED)
+    this[ABORTED] = value
+  }
+  setImpossible(value = true) {
+    this[IS_IMPOSSIBLE] = value
+  }
+  
+  isImpossible() {
+    return this[IS_IMPOSSIBLE]
+  }
+  
+  getPromise() {
+    return this.promise
+  }
+  
+  createPromise() {
+    const newPromise = new Promise((resolve, reject) => {
+          this._resolve = resolve
+          this._reject = reject
+        })
+    if (this.getPromiseCatcher) {
+      newPromise.catch((error) => this.getPromiseCatcher()(error))
+    }
+    this.promise = newPromise
+  }
+  
+  increaseCosts(delta) {
+    this.costs += Math.max(0, delta)
+  }
+  
+  getCosts() {
+    return this.costs
+  }
+  
+  resolve (...args) {
+    this._resolve(...args)
+    this.createPromise()
+  }
+  reject (...args) {
+    this._reject(...args)
+    this.createPromise()
+  }
+  
+  isEmpty () {
+    return this.data === EMPTY_NODE_DATA
+  }
 
-const counter = (currentValue => () => {
-  return currentValue++
-})(0)
-
-const defaultToId = task => {
-  if (typeof task.toId === 'function') return task.toId()
-  return R.path(['id'], task) || `id_${counter()}`
-}
-
-const defaultGetDependencyIds = task => {
-  return R.pathOr([], ['dependencies'], task)
-}
-
-const taskToTree = R.curry((toId, getDependencyIds, task) => {
-  const tree = new Tree(toId(task), task)
-  const dependecyIds = getDependencyIds(task)
-  tree.setBefore(dependecyIds)
-  return tree
-})
-
-const getScenario = R.curry((callbacks, tasks) => {
-  try {
-    const {
-      toId = defaultToId,
-      getDependencyIds = defaultGetDependencyIds
-    } = callbacks
-    const trees = tasks.map(taskToTree(toId, getDependencyIds))
+  setCatchErrorBy(getFunc) {
+    this.getPromiseCatcher = getFunc
+  }
+  
+  addErrorChild (node = Node.EMPTY, costs = 1) {
+    if (this.isAborted()) return
+    if (this.isEmpty()) return
+    this.errorChildren.push({ node, costs })
+  }
+  
+  addChild (node = Node.EMPTY, costs = 1) {
+    if (this.isAborted()) return
+    if (this.isEmpty()) return
+    this.children.push({ node, costs })
+  }
+  
+  async takeMoney (state, money, doAction = DO_NOTHING) {
+    if (this.isAborted()) this.reject(new Error('ABORTED'))
+    if (this.isEmpty()) {
+      this.resolve()
+      return
+    }
+    money = Math.min(money, this.maxGive)
+    this.money += money
+    if (this.money < this.costs) return
     
-    const forest = new Forest().appendTrees(trees)
-    const scenario = forest.toScenario
-    return scenario
+    const amount = this.costs !== 0
+      ? Math.floor(this.money / this.costs)
+      : 1
+    let nextNodes = this.children
+    try {
+      const res = await Promise.race([
+        doAction(this.data, state, amount),
+        this.abortedPromise
+      ])
+      if (res === ABORTED) {
+        return
+      }
+      this.resolve(res)
+    } catch (error) {
+      if (!this.errorChildren.length) {
+        this.reject(error)
+        return
+      }
+      state[ERRORS_PROP] = state[ERRORS_PROP] || {}
+      state[ERRORS_PROP][this.id] = state[ERRORS_PROP][this.id] || []
+      state[ERRORS_PROP][this.id].push(error)
+      nextNodes = this.errorChildren
+    }
+    this.money -= this.costs * amount
+    for (const { node, costs } of nextNodes) {
+      node.takeMoney(state, costs, doAction)
+    }
+  }
+}
+Node.EMPTY = new Node(0, EMPTY_NODE_DATA, () => EMPTY_NODE_DATA, 1, 0)
+const createErrorNode = (handler, getErrorPromiseReject) => {
+  const res = new Node(-1, {
+    [ERROR_TYPE_PROP]: ERROR_CATCHER,
+    handler
+  }, 1, 0, getErrorPromiseReject)
+  return res
+}
+const doTasks = R.curry(async (
+    tasks, 
+    {
+      getId = (counter => () => counter++)(0),
+      getDependencies = () => [],
+      getCatchers = ()=>[],
+      timeout
+    }, 
+    doAction
+  ) => {  
+  const nodes = tasks.map(data => new Node(getId(data), data, 0, 0))
+  const nodesById = R.map(R.head, R.groupBy(R.pipe(R.prop('data'), getId), nodes))
+  let errorPromiseReject = () => {}
+  let errorPromise = new Promise((resolve, reject) => {
+    errorPromiseReject = reject
+  })
+  const _doAction  = async (data, state, amount) => {
+    const isErrorData = R.prop(ERROR_TYPE_PROP, data) === ERROR_CATCHER
+    try {
+      if (isErrorData) {
+        const res = await (typeof data.handler === 'function'
+          ? data.handler({ data, state, amount, getError })
+          : data.handler)
+        return res
+      }
+      await doAction(data, state, amount)
+    } catch (err) {
+      throw err
+    }
+  }
+  nodes.forEach(node => {
+    const dependencies = getDependencies(node.data) || []
+    const catchers = getCatchers(node.data)
+    dependencies.forEach(id => {
+      const parent = nodesById[id]
+      if (!parent) {
+        node.setImpossible()
+        return
+      }
+      nodesById[id].addChild(node, 1)
+      node.increaseCosts(1)
+    })
+    catchers.forEach(func => {
+      const errorNode = createErrorNode(func, () => errorPromiseReject)
+      node.addErrorChild(errorNode, 1)
+    })
+  })
+  if (nodes.some(node => node.isImpossible())) {
+    throw new Error('Impossible to solve these tasks')
+  }
+  const nodePromises = Promise.all(nodes.map(node => node.getPromise()))
+
+  const actualTimeout = typeof timeout === 'function'
+    ? timeout({ tasks })
+    : timeout
+  const timeoutPresent = typeof actualTimeout === 'number' && actualTimeout > 0
+  const promises = timeoutPresent
+    ? createTimeoutPromise(actualTimeout, nodePromises)
+    : nodePromises
+  const state = {}
+  
+  try {
+    await Promise.all([errorPromise, ...nodes.filter(node => node.getCosts() === 0)
+      .map(n => n.takeMoney(state, 1, _doAction))])
+    res = await promises
+
+    return {
+      res,
+      state
+    }
   } catch (error) {
+    nodes.forEach(node => node.setAborted())
     throw error
   }
+  
 })
-
-const toSchema = scenario => {
-  const braces = {
-    [ORDER_TYPES.EMPTY]: str => str,
-    [ORDER_TYPES.ONE_ACTION]: str => str,
-    [ORDER_TYPES.SEQUENCE]: str => `(${str})`,
-    [ORDER_TYPES.PARALLEL]: str => `[${str}]`
-  }
-  const getText = {
-    [ORDER_TYPES.EMPTY]: s => '',
-    [ORDER_TYPES.ONE_ACTION]: s => s.task.id,
-    [ORDER_TYPES.SEQUENCE]: s => s.scenarios.map(toSchema).join(', '),
-    [ORDER_TYPES.PARALLEL]: s => s.scenarios.map(toSchema).join(', ')
-  }
-  const t = scenario.type
-  return braces[t](getText[t](scenario))
+module.export = {
+  doTasks // async (tasks, { getId, getDependencies, getCatchers, timeout}, doAction: function(data, state, amount): *)
 }
 
-async function doScenario (getPromiseFromTask, scenario) {
-  try {
-    switch (scenario.type) {
-      case ORDER_TYPES.EMPTY:
-        return
-      case ORDER_TYPES.ONE_ACTION:
-        return await getPromiseFromTask(scenario.task)
-      case ORDER_TYPES.SEQUENCE:
-        return await scenario.scenarios.reduce((previous, cur)=>{
-          return previous.then(() => doScenarioAsync(getPromiseFromTask, cur))
-        }, Promise.resolve())
-      case ORDER_TYPES.PARALLEL:
-        return await Promise.all(
-          scenario.scenarios.map(sc => doScenarioAsync(getPromiseFromTask, sc))
-        )
-    }
-  } catch (error) {
-    throw error
-  }
-  return 'Something'
-}
-
-function doScenarioSync (doAction, scenario) {
-  try {
-    switch (scenario.type) {
-      case ORDER_TYPES.EMPTY:
-        return
-      case ORDER_TYPES.ONE_ACTION:
-        return doAction(scenario.task)
-      case ORDER_TYPES.SEQUENCE:
-      case ORDER_TYPES.PARALLEL:
-        scenario.scenarios.forEach(doAction)
-    }
-  } catch (error) {
-    throw error
-  }
-}
-
-module.exports = {
-  ORDER_TYPES,
-  getScenario,
-  toSchema,
-  doScenario,
-  doScenarioSync
-}
+// const tasks = [
+//   { id: 0, time: 50, dependencies: [3] },
+//   { id: 1, time: 100, dependencies: []},
+//   { id: 2, time: 200, dependencies: [0] },
+//   { id: 3, time: 400, dependencies: [1] },
+//   { id: 4, time: 800, dependencies: [2] },
+//   { id: 5, time: 700, dependencies: [2, 3], exception: 'ERROR' }, // comment it
+//   { id: 6, time: 700, dependencies: [2, 3]}
+// ]
+// const getId = R.prop('id')
+// const getDependencies = R.prop('dependencies')
+// const getCatchers = ({ id }) => [({state, getError })=>{
+//   const errors = getError(state, id)
+//   console.log('in catcher: ', errors)
+//   // throw 'exception'
+// }]
+// const doAction = async (data, state, amount) => {
+//   if (!state.order) state.order = []
+//   if (data.exception) {
+//     throw new Error(data.exception)
+//   }
+//   for (let i = 0; i < amount; i++) {
+//     await wait(data.time)
+//     state.order.push(data.id)
+//     console.log(data.id)
+//   }
+//   return data.id
+// }
+// async function main() {
+//   try {
+//     const res = await doTasks(tasks, { getId, getDependencies, getCatchers }, doAction)
+//     console.log(res)
+//   } catch (err) {
+//     console.log('catched ERROR: ', err)
+//   }
+// }
+// console.clear()
+// main()// 
